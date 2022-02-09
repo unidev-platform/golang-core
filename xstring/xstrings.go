545d8326843cc292f2fcdb534ba11941dbf53a5e
denis256 <denis.o@linux.com>
2022-02-10 00:38:42 +0200

    Added string functions

--- xstring/xstrings.go
+++ xstring/xstrings.go
@@ -1,9 +1,26 @@
 package xstring
 
+import "strings"
+
 // Between - extract from input string all substrings located between begin and end.
 // Useful for extracting data between HTML tags.
 func Between(input string, begin string, end string) []string {
 	var result []string
+
+	for {
+		beginIndex := strings.Index(input, begin)
+		if beginIndex == -1 {
+			break
+		}
+		part := input[beginIndex+len(begin):]
+		endIndex := strings.Index(part, end)
+		if endIndex == -1 {
+			break
+		}
+		finalPart := part[:endIndex]
+		result = append(result, finalPart)
+	}
+
 	return result
 
 }
--- xstring/xstrings_test.go
+++ xstring/xstrings_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestStringExtractions(t *testing.T) {
-	items := ExtractStringBetween(" 1 qwe 2  1 xxx 2", "1", "2")
+	items := Between(" 1 qwe 2  1 xxx 2", "1", "2")
 
 	assert.Equal(t, 2, len(items))
 
